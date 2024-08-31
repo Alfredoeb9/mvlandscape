ce9619271012ee301e76b50ef0aed4ca1ad04ba8
Alfredo Barillas <32616945+Alfredoeb9@users.noreply.github.com>
2024-08-31 16:17:49 -0700

    Delete src directory

--- src/routes/services/Residential.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/services/Residential')({
-  component: () => <div>Hello /services/Residential!</div>
-})
