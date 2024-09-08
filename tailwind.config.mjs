bdd70b584567f62c0443266a78c41939486902ec
Alfredoeb9 <alfredoeb96@gmail.com>
2024-09-08 09:29:28 -0700

    [css] updated css for index page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        primary: "#45a342",
+      },
+    },
+  },
+  plugins: [],
+};
