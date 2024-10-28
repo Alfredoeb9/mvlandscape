5dff45e458ba9588eef59fc3566df0f6ddfe224e
Alfredoeb9 <alfredoeb96@gmail.com>
2024-10-28 10:40:45 -0700

    [contact & index] updated contact security and added in portfolio images

--- src/pages/contactform.php
+++ src/pages/contactform.php
@@ -13,13 +13,14 @@ if(isset($_POST['submit'])) {
     $subject = $_POST['subject'];
     $mailFrom = $_POST['email'];
     $message = $_POST['message'];
+    $ipaddress = $_SERVER['REMOTE_ADDR'];
 
     // Message to me
     //$mailTo = "alfredoeb96@gmail.com":
-    $headers = "From: alfredoeb96@gmail.com";
+    $headers = "From: ".$mailFrom;
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; carset=iso-8859-1\n";
-    $txt = "You have received an e-mail from ".$mailFrom.".\r\n".$subject."\r\n".$message;
+    $txt = "You have received an e-mail from IPADDRESS: ".$ipaddress." \r\n Email: ".$mailFrom.".\r\n Subject: ".$subject."\r\n Message:".$message;
 
     //try {
             //Server settings
@@ -27,13 +28,13 @@ if(isset($_POST['submit'])) {
         $mail->isSMTP();                                            // Set mailer to use SMTP
         $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = $_ENV["EMAIL_HANDLE"];                  // SMTP username
-        $mail->Password   = $_ENV["EMAIL_PWD"].;                    // SMTP password
+        $mail->Username   = $_ENV["EMAIL_HANDLE"];                  // SMTP username use getenv() to get the value in production
+        $mail->Password   = $_ENV["EMAIL_PWD"].;                    // SMTP password use getenv() to get the value in production
         $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, [ICODE]ssl[/ICODE] also accepted
-        $mail->Port       = $_ENV["EMAIL_PORT"];                    // TCP port to connect to
+        $mail->Port       = $_ENV["EMAIL_PORT"];                    // TCP port to connect to use getenv() to get the value in production
 
         //Recipients
-        $mail->setFrom("test@gmail.com", 'Mailer');
+        $mail->setFrom("support@mvlandscape.net", 'Mvlandscape');
         $mail->addReplyTo('alfredoeb96@gmail.com', 'Information');
         $mail->addCC('alfredoeb96@gmail.com');                             // This is needed for production
         //$mail->addBCC('bcc@example.com');
@@ -46,11 +47,12 @@ if(isset($_POST['submit'])) {
         $mail->isHTML(true);                                  // Set email format to HTML
         $mail->Subject = $subject;
         $mail->Body    = $txt;
-        $mail->AltBody = $txt;
 
-        $mail->send();
-        header("Location: /");
-        echo 'Message has been sent';
+        if ($mail->send()) {
+          header("Location: /");
+        } else {
+          echo "Message could not be sent. Mailer Error";
+        }
     //}
     //} catch (Exception $e) {
       //  echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
